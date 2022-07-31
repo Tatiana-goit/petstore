ebe69b07ea398e8da971032b0f9ca33e8c484b5b
Tatiana-goit <tanchou74@gmail.com>
2022-08-01 00:06:55 +0300

    added MakeOrder

--- src/pages/Personal/Personal.tsx
+++ src/pages/Personal/Personal.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Container from '../../components/Container/Container'
 import FindOrder from '../../components/FindOrder/FindOrder'
+import MakeOrder from '../../components/MakeOrder/MakeOrder'
 
 export default function Personal() {
   return (
     <Container>
-      <div>Personal</div>
-      <FindOrder/>
+      <div>Personal page</div>
+      <FindOrder />
+      <MakeOrder />
     </Container>
   )
 }
---
+++ src/services/OrderApi.jsx
@@ -0,0 +1,14 @@
+import axios from 'axios'
+axios.defaults.baseURL = 'https://petstore3.swagger.io/api/v3'
+
+export async function findOrderById(id) {
+    try {
+      const result = await axios.get(`store/order/${id}`)
+      console.log(result.data);
+      return result.data
+    } catch (error) {
+      console.log(error.message)
+    //   alert(`Number "${id}" does not exist, please enter a valid number`)
+    }
+  }
+  

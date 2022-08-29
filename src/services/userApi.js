eeb1f787fee2cc9b4ca161c21a387392aec4d334
Tatiana-goit <tanchou74@gmail.com>
2022-08-29 23:28:48 +0300

    delete other files

--- src/services/userApi.js
+++ src/services/userApi.js
@@ -1,36 +1,36 @@
-import axios from 'axios';
-axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
+import axios from 'axios'
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com'
 
 const token = {
   set(token) {
-    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`
   },
   unset() {
-    axios.defaults.headers.common.Authorization = '';
+    axios.defaults.headers.common.Authorization = ''
   },
-};
+}
 
 export async function register(user) {
-  const result = await axios.post('/users/signup', user);
-  console.log("register", result);
-  token.set(result.data.token);
-  return result.data;
+  const result = await axios.post('/users/signup', user)
+  console.log('register', result)
+  token.set(result.data.token)
+  return result.data
 }
 
 export async function login(user) {
-  const result = await axios.post('/users/login', user);
-  console.log("login", result);
-  token.set(result.data.token);
-  return result.data;
+  const result = await axios.post('/users/login', user)
+  console.log('login', result)
+  token.set(result.data.token)
+  return result.data
 }
 
 export async function currentUser(userToken) {
-  token.set(userToken);
-  const result = await axios.get('/users/current');
-  return result.data;
+  token.set(userToken)
+  const result = await axios.get('/users/current')
+  return result.data
 }
 
 export async function logout() {
-  await axios.post('/users/logout');
-  token.unset();
+  await axios.post('/users/logout')
+  token.unset()
 }
